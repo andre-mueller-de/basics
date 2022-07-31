9db5b000c44e975ae9e8eb0b54cccb98acc01359
andre-mueller-de <51235668+andre-mueller-de@users.noreply.github.com>
2022-07-31 13:21:55 +0200

    Create test1.sh

---
+++ bash/test1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# >>> pipefail <<<
+# -e -> fail script immediatly after command fails (returns non 0 exit code)
+#       will not fail for branch in conditional statement
+# -o -> exit if any command in pipeline fails
+# -u -> exit on unset variables
+# -x -> prints out command before execution
+set -euo pipefail
